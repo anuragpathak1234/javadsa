97c566ac3a86a0fa05d0fce0b07b76428dd0d410
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-02 16:46:05 +0530

    learn primitive data types

--- basicjava/Input.java
+++ basicjava/Input.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 public class Input {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-    System.out.println(input.nextInt());
+    System.out.println(input.nextLine());
   }
 }
