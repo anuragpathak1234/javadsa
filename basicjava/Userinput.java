0260a8bbb46685d84212c227affef394d134e081
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-03 14:07:00 +0530

    learn about how to take integer userinput

---
+++ basicjava/Userinput.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Userinput {
+  public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+
+      int rollno  = input.nextInt();
+      System.out.println("your roll no is " + rollno);
+
+  }
+  
+}
---
+++ basicjava/Usrint2.java
@@ -0,0 +1,2 @@
+public class Usrint2 {
+}
