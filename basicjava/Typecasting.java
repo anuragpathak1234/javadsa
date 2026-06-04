cef424cec8a24c9b601abc09746354b638f76eeb
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-04 11:07:46 +0530

    learn about basic of java

---
+++ basicjava/Typecasting.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Typecasting {
+    public static void main(String[] args){
+
+//        Scanner input = new Scanner(System.in);
+
+        // typecasting
+//        int num = (int)(78.88f);
+//        System.out.println(num);
+
+        // automatic type promotions in expressions
+
+        // we have to keep in mind that byte can maximum stores 256 range value but when we use it in expression it can store larger value due to
+        // automatic type promotions in expressions
+
+//        int a = 257;
+//        byte b = (byte)(a);
+//
+//        System.out.println(b);
+
+//        byte a = 20;
+//        byte b = 20;
+//        byte c = 20;
+//
+//        int d = a * b / c;
+//        System.out.println(d);
+//          int number = 'A';
+//          System.out.println(number);
+
+//      if condition
+//        if (false){
+//            System.out.println("Hello world");
+//        }
+
+        // using while loop
+//        int count  = 1;
+//        while(count != 5){
+//            System.out.println("Hello Chhotu");
+//            count += 1;
+//        }
+
+
+
+        // for loop
+//        for(int i = 1; i < 5; i++){
+//            System.out.println("Hello World");
+//        }
+
+    }
+}
