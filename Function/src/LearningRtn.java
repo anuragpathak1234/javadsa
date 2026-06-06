4e974424cfb1ed3ef4f572f1c9d32ecb57b7f785
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-06 12:34:49 +0530

    learn about retuen in methods

---
+++ Function/src/LearningRtn.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LearningRtn {
+
+    public static void main(String[] args){
+        System.out.println(sum());
+
+    }
+
+    static int sum(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter num1 : ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter num2 : ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        return sum;
+
+    }
+}
