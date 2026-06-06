a1d4a955d2ffd6e16611d4082cbbf370709d442b
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-06 12:25:15 +0530

    leanr the basic of method/functions in java

---
+++ Function/src/BasicFunction.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BasicFunction {
+    static void  greet(){
+        System.out.println("Hello Anurag");
+    }
+    public static void main(String[] args){
+        greet();
+    }
+}
