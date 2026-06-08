56194bf22b7e3186c80876da3363ffd2ee4b7a23
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-08 11:06:50 +0530

    learn about variable arguments

--- Function/src/BasicFunction.java
+++ Function/src/BasicFunction.java
@@ -1,10 +1,26 @@
-import java.util.Scanner;
-
 public class BasicFunction {
-    static void  greet(){
-        System.out.println("Hello Anurag");
+    static void greet() {
+        System.out.println("Hello!");
     }
-    public static void main(String[] args){
+
+    static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    static void greet(String name, int times) {
+        for (int i = 1; i <= times; i++) {
+            System.out.println(i + ". Hello, " + name + "!");
+        }
+    }
+
+    static void greet(int age) {
+        System.out.println("You are " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
         greet();
+        greet("Anurag");
+        greet("Anurag", 3);
+        greet(20);
     }
 }
