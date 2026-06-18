fee7e4525d01e9602ec9dcb670ec96d575b886e8
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-18 14:46:23 +0530

    Learn about how to take input and output in 1D-Arrays

--- MethodsAssiments/src/PrimeNumByMethod.java
+++ MethodsAssiments/src/PrimeNumByMethod.java
@@ -1,33 +1,84 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+//
+//public class PrimeNumByMethod {
+//    public static void main(String[] args){
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Enter your num : ");
+//        int num = input.nextInt();
+//
+//        boolean ans  = isPrime(num);
+////        System.out.println(ans);
+//        for(int i = 2; i < num; i++){
+//            if(isPrime(i)){
+//                System.out.print(i + " ");
+//            }
+//        }
+//    }
+//
+//    static boolean isPrime(int num){
+//        if(num == 1 && num == 0){
+//            return false;
+//        }
+//
+//        for(int i = 2; i < num; i++){
+//            if(num % i == 0){
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+//}
 
-public class PrimeNumByMethod {
+
+
+
+
+import java.util.Scanner:
+
+public class PrimeNumByMethod{
     public static void main(String[] args){
-
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter your num : ");
-        int num = input.nextInt();
+        System.out.print("Enter num : ");
+        int n = input.nextInt();
 
-        boolean ans  = isPrime(num);
-//        System.out.println(ans);
-        for(int i = 2; i < num; i++){
-            if(isPrime(i)){
-                System.out.print(i + " ");
-            }
-        }
+        boolean ans = isPrime(n);
+        System.out.println(ans);
     }
 
-    static boolean isPrime(int num){
-        if(num == 1 && num == 0){
+    static boolean isPrime(int n){
+
+        if(n == 1 && n == 0){
             return false;
         }
 
-        for(int i = 2; i < num; i++){
-            if(num % i == 0){
+        for(int i = 2; i <= n; i++){
+            if(n % i == 0 ){
                 return false;
             }
         }
-
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
