4a7e6b289a085fba65c02263b3671660d0008016
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-20 13:27:48 +0530

    Learn how to Search Char in String Arrays

---
+++ LinearSerachAlgorithm/src/LinearSearchInString.java
@@ -0,0 +1,31 @@
+public class LinearSearchInString {
+    public static void main(String[] args){
+        String name = "chhotu";
+        char target = 'u';
+
+        boolean ans = search(name,target);
+        System.out.println(ans);
+    }
+
+    static boolean search(String name, char target){
+        if(name.length() == 0){
+            return false;
+        }
+
+//        for(int i = 0; i < name.length();i++){
+//            if(name.charAt(i) == target){
+//                return true;
+//            }
+//        }
+//
+//        return false;
+
+        for(char ch : name.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
