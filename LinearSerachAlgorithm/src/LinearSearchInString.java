41fbda16b7885d1598efa1ee08ea34b2943f9de0
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-06-20 18:06:25 +0530

    Learn how to serch target value in specified range

--- LinearSerachAlgorithm/src/LinearSearchInString.java
+++ LinearSerachAlgorithm/src/LinearSearchInString.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 public class LinearSearchInString {
     public static void main(String[] args){
         String name = "chhotu";
         char target = 'u';
+
+        System.out.println(Arrays.toString(name.toCharArray()));
 
         boolean ans = search(name,target);
         System.out.println(ans);
